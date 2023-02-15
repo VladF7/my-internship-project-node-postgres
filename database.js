8307cae71f69ef649e0743e43bc94aecaae01175
VladF7 <v.furnika@gmail.com>
2023-02-15 21:42:47 +0200

    initial commit

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    host: 'localhost',
+    host: 'https://internship-backend.onrender.com',
     user: 'postgres',     
     password: 'root',
     database: 'my-internship-db',
