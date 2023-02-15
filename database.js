f780a8ac7298f377619a62104cfcb1c127e18c55
VladF7 <v.furnika@gmail.com>
2023-02-15 20:59:30 +0200

    initial commit

--- database.js
+++ database.js
@@ -7,12 +7,4 @@ const pool = new Pool({
     port: 5432,
 })
 
-pool.connect((err)=>{
-    if(err){
-        console.log(err)
-        return err
-    }
-    console.log('db connected')
-})
-
 module.exports = pool
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = require('./routes/mainRouter')
-
+const pool = require('./database')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
 
 app.use('/api', router)
+
+pool.connect((err)=>{
+    if(err){
+        console.log(err)
+        return err
+    }
+    console.log('db connected')
+})
 
 app.listen(PORT,()=> console.log('Server started on PORT'+ ' ' + PORT ))
 
