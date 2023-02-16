190f06e038379f9fdfaa46d62bc8c2e2d00b254c
VladF7 <v.furnika@gmail.com>
2023-02-16 11:59:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = require('./routes/mainRouter')
 const app = express()
 const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
+const corsOptions = require('./config/corsOption')
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
