4d150ed8f9f072e6cbf28916a3183a87738e0bd1
VladF7 <v.furnika@gmail.com>
2023-06-01 15:06:03 +0300

    Update orders.model.js

--- models/orders.model.js
+++ models/orders.model.js
@@ -30,7 +30,7 @@ export const sortByFields = {
 
 export const sortOptions = ['asc', 'desc']
 export const limitOptions = [10, 25, 50]
-export const statusFilterOptions = ['Completed', 'Confirmed', 'Canceled', '']
+export const statusFilterOptions = ['Completed', 'Await Payment', 'Payment Success', 'Canceled', '']
 const serverTimezoneOffset = new Date().getTimezoneOffset()
 
 export default {
